e85166a3f69079a260e9c3c855e2e54832aee6cd
clarabranches <pcbranches@gmail.com>
2026-04-13 10:47:36 -0300

    Login

--- js/firebase.js
+++ js/firebase.js
@@ -323,3 +323,20 @@ window.editarAny = function(id, nomeAtual, idAtual, event) {
         id: novoId
     });
 };
+
+/* ================================
+   LOGIN
+================================ */
+function login() {
+  const email = document.getElementById("email").value;
+  const senha = document.getElementById("senha").value;
+
+  signInWithEmailAndPassword(auth, email, senha)
+    .then(userCredential => {
+      alert("Login realizado!");
+      window.location.href = "painel.html"; // página protegida
+    })
+    .catch(error => {
+      alert("Erro: " + error.message);
+    });
+}
